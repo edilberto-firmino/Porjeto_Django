beaf1d79ed02d3b01a208a402b7de190ae5ca63c
edilberto-firmino <ediberto_ti@yahoo.com.br>
2022-02-18 21:19:36 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,3 @@ from django.shortcuts import HttpResponse, render
 
 def home(resquest):
     return render(resquest, 'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
